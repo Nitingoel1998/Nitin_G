80756a5706b93ebc84777d0bb2907c24b51be72c
nitin.goel <nitin.goel@inbl01lap5535.brd.g-c-i.com>
2023-10-17 18:32:30 +0530

    page Factory added

---
+++ src/main/java/PageObjects/pagefactory.java
@@ -0,0 +1,50 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class pagefactory {
+
+	public pagefactory(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+
+	}
+	
+	@FindBy (id = "Email")
+	private WebElement username;
+	
+	@FindBy (id = "Password")
+	private WebElement passwordinLogin;
+
+	@FindBy (xpath = "//*[text()='Log in']")
+	private WebElement login;
+	
+	@FindBy (xpath = "(//input[@type='submit'])[2]")
+	private WebElement click;
+
+
+public WebElement getusername() {
+		return username;
+	}
+public WebElement getclick() {
+	return click;
+}
+
+	public WebElement getpasswordinLogin() {
+		return passwordinLogin;
+	}
+
+public WebElement login() {
+		return login;
+	}
+@FindBy (id = "register-button")
+private WebElement save;
+
+
+public WebElement getSave() {
+	return save;
+}
+
+}
