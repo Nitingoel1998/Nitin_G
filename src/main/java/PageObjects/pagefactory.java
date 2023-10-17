0f135d5c93e8e3fe30a9232f2ea08612ddf80a7c
Nitin Goel <nitin.goel@collabera.com>
2023-10-17 19:10:20 +0530

    added page factory

--- src/main/java/PageObjects/pagefactory.java
+++ src/main/java/PageObjects/pagefactory.java
@@ -5,14 +5,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class pagefactory{
+public class pagefactory  {
 
-	public pagefactory(WebDriver driver) {
+	public pagefactory(WebDriver driver ) {
 		PageFactory.initElements(driver, this);
 
 	}
 	
-
 	@FindBy (id = "Email")
 	private WebElement username;
 	
@@ -25,34 +24,13 @@ public class pagefactory{
 	@FindBy (xpath = "(//input[@type='submit'])[2]")
 	private WebElement click;
 
-         @FindBy(xpath="(//a[contains(text(),'Electronics')])[3]")
-      private WebElement Electronics;
-         @FindBy(xpath="(//*[@title='Show products in category Cell phones'])[3]")
-         private WebElement cellphone;
-         
-         @FindBy(xpath="(//input[@type='button'])[3]")
-          private WebElement AddToCart;
-         
+
 public WebElement getusername() {
 		return username;
 	}
-
-public WebElement getElectronics() {
-	return Electronics;
-}
-
-public WebElement getcellphone() {
-	return cellphone;
-}
-
-public WebElement getaddtocart() {
-	return AddToCart;
-}
 public WebElement getclick() {
 	return click;
 }
-
-	
 
 	public WebElement getpasswordinLogin() {
 		return passwordinLogin;
@@ -60,17 +38,13 @@ public WebElement getclick() {
 
 public WebElement login() {
 		return login;
-
 	}
 @FindBy (id = "register-button")
 private WebElement save;
 
 
-
-
-
-
-
-
+public WebElement getSave() {
+	return save;
+}
 
 }
